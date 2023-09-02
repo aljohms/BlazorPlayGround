9c534d21a61255b2c6e30709599c732489fac8b7
MOHAMMED ALJOHANI <aljohms1@gmail.com>
2023-09-03 01:52:37 +0300

    modify grid to add event call back and simplify display cols

---
+++ BlazorPlayGround/Client/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorPlayGround.Client;
+
+public class Car
+{
+    public string Make { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public int Year { get; set; }
+}
--- BlazorPlayGround/Client/GridColumn.cs
+++ BlazorPlayGround/Client/GridColumn.cs
@@ -1,9 +1,34 @@
 ﻿
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace BlazorPlayGround.Client;
 public class GridColumn<T>
 {
     public PropertyInfo Property { get; set; }
     public string Title { get; set; }
+//construct
+    public GridColumn(PropertyInfo property)
+    {
+        Property = property;
+        Title = property.Name;
+    }
+    public GridColumn( string title, PropertyInfo property)
+    {
+        Property = property;
+        Title = title;
+    }    
+    public GridColumn( string title, string propertyName)
+    {
+
+        Property = typeof(T).GetProperty(propertyName);
+        Title = title;
+    }
+    public object GetValue(T item)
+    {
+        return Property.GetValue(item);
+    }
+
 }
+
+
